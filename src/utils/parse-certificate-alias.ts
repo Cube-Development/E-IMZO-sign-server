4457d3725f76871598cc569803ce2a0709e65e02
KDF25 <karimov.damir.faridovich@gmail.com>
2025-09-08 15:43:04 +0500

    fix: fix cert alias

--- src/utils/parse-certificate-alias.ts
+++ src/utils/parse-certificate-alias.ts
@@ -13,6 +13,7 @@ export const parseCertificateAlias = (alias: string): ParsedCertificateInfo => {
 
   // Извлекаем ПИНФЛ из поля 1.2.860.3.16.1.2
   const pnfl = info["1.2.860.3.16.1.2"] || "No data";
+  const uid = info["1.2.860.3.16.1.1"] || info.uid || null;
 
   // Форматируем дату - берем только день.месяц.год
   const formatDate = (dateStr: string) => {
@@ -32,9 +33,9 @@ export const parseCertificateAlias = (alias: string): ParsedCertificateInfo => {
     surname: info.surname || "No data",
     location: info.l || "No data",
     region: info.st || "No data",
-    uid: info?.uid || null,
-    organization: info.o || "No data",
+    organization: info.o || "",
     pnfl: pnfl,
+    uid: uid,
     validFrom: formatDate(info.validfrom),
     validTo: formatDate(info.validto),
   };
